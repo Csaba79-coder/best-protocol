1f3484fddfb754a2c74d831ad88946ef5de02bf7
Csaba79-coder <csabavadasz79@gmail.com>
2023-03-02 20:42:18 +0100

    create Repository for Representative Entity

---
+++ government-service/src/main/java/com/csaba79coder/bestprotocol/model/representative/persistence/RepresentativeRepository.java
@@ -0,0 +1,7 @@
+package com.csaba79coder.bestprotocol.model.representative.persistence;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RepresentativeRepository {
+}
