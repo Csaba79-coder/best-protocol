8ed3323c497a8b354aeac1cccae915268dd6f78b
Csaba79-coder <csabavadasz79@gmail.com>
2023-03-02 23:06:32 +0100

    implement GovernmentRepresentativeApi from api-contract module, and necessary dependencies and make setups

--- government-service/src/main/java/com/csaba79coder/bestprotocol/controller/RepresentativeController.java
+++ government-service/src/main/java/com/csaba79coder/bestprotocol/controller/RepresentativeController.java
@@ -1,7 +1,30 @@
 package com.csaba79coder.bestprotocol.controller;
 
+import com.csaba79coder.api.GovernmentRepresentativeApi;
+import com.csaba79coder.bestprotocol.model.representative.service.RepresentativeService;
+import com.csaba79coder.model.NewRepresentativeModel;
+import com.csaba79coder.model.RepresentativeModel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-public class RepresentativeController {
+@CrossOrigin(value = "http://localhost:4200")
+@RequiredArgsConstructor
+public class RepresentativeController implements GovernmentRepresentativeApi {
+
+    private final RepresentativeService representativeService;
+
+    @Override
+    public ResponseEntity<List<RepresentativeModel>> renderAllRepresentatives() {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<RepresentativeModel> representativeApiAddNewRepresentative(NewRepresentativeModel body) {
+        return null;
+    }
 }
