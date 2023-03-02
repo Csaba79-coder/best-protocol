d468aa2472d4dc3372ab24d39c320c418f4edcbd
Csaba79-coder <csabavadasz79@gmail.com>
2023-03-02 23:56:56 +0100

    creating RepresentativeService for the controller, that add a new Representative and renderAllRepresentative

--- government-service/src/main/java/com/csaba79coder/bestprotocol/model/representative/service/RepresentativeService.java
+++ government-service/src/main/java/com/csaba79coder/bestprotocol/model/representative/service/RepresentativeService.java
@@ -1,7 +1,29 @@
 package com.csaba79coder.bestprotocol.model.representative.service;
 
+import com.csaba79coder.bestprotocol.model.representative.persistence.RepresentativeRepository;
+import com.csaba79coder.bestprotocol.util.mapper.Mapper;
+import com.csaba79coder.model.NewRepresentativeModel;
+import com.csaba79coder.model.RepresentativeModel;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
+@RequiredArgsConstructor
 public class RepresentativeService {
+
+    private final RepresentativeRepository representativeRepository;
+
+    public RepresentativeModel addNewRepresentative(NewRepresentativeModel body) {
+        return Mapper.mapRepresentativeEntityToModel(representativeRepository.save(Mapper.mapNewRepresentativeModelToEntity(body)));
+    }
+
+    public List<RepresentativeModel> renderAllRepresentatives() {
+        return representativeRepository.findAll()
+                .stream()
+                .map(Mapper::mapRepresentativeEntityToModel)
+                .collect(Collectors.toList());
+    }
 }
