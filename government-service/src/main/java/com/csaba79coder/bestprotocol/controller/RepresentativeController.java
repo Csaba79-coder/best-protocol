f7db1f9d04838a269d1c0c9b4cb295c89f212650
Csaba79-coder <csabavadasz79@gmail.com>
2023-03-02 23:58:17 +0100

    create and implement method in Controller, get all method works with Postman

--- government-service/src/main/java/com/csaba79coder/bestprotocol/controller/RepresentativeController.java
+++ government-service/src/main/java/com/csaba79coder/bestprotocol/controller/RepresentativeController.java
@@ -2,12 +2,12 @@ package com.csaba79coder.bestprotocol.controller;
 
 import com.csaba79coder.api.GovernmentRepresentativeApi;
 import com.csaba79coder.bestprotocol.model.representative.service.RepresentativeService;
-import com.csaba79coder.model.NewRepresentativeModel;
 import com.csaba79coder.model.RepresentativeModel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -20,11 +20,11 @@ public class RepresentativeController implements GovernmentRepresentativeApi {
 
     @Override
     public ResponseEntity<List<RepresentativeModel>> renderAllRepresentatives() {
-        return null;
+        return ResponseEntity.status(200).body(representativeService.renderAllRepresentatives());
     }
 
     @Override
-    public ResponseEntity<RepresentativeModel> representativeApiAddNewRepresentative(NewRepresentativeModel body) {
+    public ResponseEntity<RepresentativeModel> representativeApiAddNewRepresentative(String name, String jobTitle, String address, String phoneNumber, String email, MultipartFile image, String note) {
         return null;
     }
 }
