cfc12dcfc40310b63c02f6323ccca70cd68a6cf7
Csaba79-coder <csabavadasz79@gmail.com>
2023-03-11 12:15:43 +0100

    creating a component for representatives

---
+++ frontend/src/app/government-service/components/representative-list/representative-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-representative-list',
+  templateUrl: './representative-list.component.html',
+  styleUrls: ['./representative-list.component.css']
+})
+export class RepresentativeListComponent {
+
+}
