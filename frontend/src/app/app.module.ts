27afee4f975a3ddc8e6054197b72b59c15cd8457
Csaba79-coder <csabavadasz79@gmail.com>
2023-03-11 11:52:18 +0100

    importing HttpClientModule, and ApiModule in app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {ApiModule} from "../../build/openapi/governemnt-service";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
   providers: [],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ApiModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
