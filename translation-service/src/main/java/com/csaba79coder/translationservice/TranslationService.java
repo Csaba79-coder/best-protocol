bee7dff0493c31f617f80613078c7a272f7e7b76
Csaba79-coder <csabavadasz79@gmail.com>
2023-03-23 15:01:38 +0100

    creating new module for tranlsation-service

---
+++ translation-service/src/main/java/com/csaba79coder/translationservice/TranslationService.java
@@ -0,0 +1,12 @@
+package com.csaba79coder.translationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslationService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TranslationService.class, args);
+    }
+}
---
+++ translation-service/src/test/java/com/csaba79coder/translationservice/TranslationServiceTests.java
@@ -0,0 +1,13 @@
+package com.csaba79coder.translationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslationServiceTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
