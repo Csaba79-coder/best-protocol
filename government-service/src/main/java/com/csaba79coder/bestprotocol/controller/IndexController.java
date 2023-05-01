daef0f71770bc2a689e4a1ec15a9997b8c95e4e7
Csaba79-coder <csabavadasz79@gmail.com>
2023-05-01 14:20:43 +0200

    implementing endpoint for index.html, creating IndexController, and generate index.html

--- government-service/src/main/java/com/csaba79coder/bestprotocol/controller/IndexController.java
+++ government-service/src/main/java/com/csaba79coder/bestprotocol/controller/IndexController.java
@@ -3,10 +3,12 @@ package com.csaba79coder.bestprotocol.controller;
 import com.csaba79coder.bestprotocol.api.IndexApi;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(value = "http://localhost:4200")
+@RequestMapping("")
 public class IndexController implements IndexApi {
 
     private final String INDEX = "index.html";
@@ -17,7 +19,12 @@ public class IndexController implements IndexApi {
     }
 
     @Override
-    public ResponseEntity<String> renderIndexPage_0() {
+    public ResponseEntity<String> renderIndexPageIndex() {
+        return ResponseEntity.status(200).body(INDEX);
+    }
+
+    @Override
+    public ResponseEntity<String> renderIndexPageFromIndexHtml() {
         return ResponseEntity.status(200).body(INDEX);
     }
 }
